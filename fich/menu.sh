c58d336be1e22dbb0d91bf9601529f72a6a7c312
Xabier <xabierdedomingo@gmail.com>
2019-04-17 18:38:20 +0200

    La función 3 no estaba bonita en verdad, ahora sí

--- fich/menu.sh
+++ fich/menu.sh
@@ -36,18 +36,18 @@ function instalarPHP()
 	aux2=$(aptitude show libapache2-mod-php | grep "Estado: instalado")
 	if [ -z "$aux" ]
 	then 
- 	  echo "Instalando paquete PHP ...\n"
+ 	  echo -e "Instalando paquete PHP ...\n"
  	  sudo apt install php
 	else
-   	  echo "El paquete PHP ya estaba instalado\n"
+   	  echo -e "El paquete PHP ya estaba instalado\n"
  	fi
     
 	if [ -z "$aux2" ]
 	then
-	  echo "Instalando paquete libapache2-mod-php ...\n"
+	  echo -e "Instalando paquete libapache2-mod-php ...\n"
 	  sudo apt install libapache2-mod-php
 	else
-	  echo "El paquete libapache2-mod-php ya estaba instalado\n"
+	  echo -e "El paquete libapache2-mod-php ya estaba instalado\n"
 	fi 
 }
 
